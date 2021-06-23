a481c23a360acf4da0b7efcc3129e7a5f70000a5
Gustavo Silva <gstvds@icloud.com>
2021-06-22 21:46:43 -0300

    fix: Fixes some styles

--- src/components/Category/index.tsx
+++ src/components/Category/index.tsx
@@ -12,11 +12,12 @@ import { theme } from '../../global/styles/theme';
 type CategoryProps = RectButtonProps & {
   title: string;
   icon: React.FC<SvgProps>;
+  hasCheckbox?: boolean;
   checked?: boolean;
 }
 
-export function Category({ title, icon: Icon, checked = false, ...rest }: CategoryProps) {
-  const { secondary50, secondary70 } = theme.colors;
+export function Category({ title, icon: Icon, checked = false, hasCheckbox = true, ...rest }: CategoryProps) {
+  const { secondary40, secondary50, secondary70, secondary85 } = theme.colors;
 
   return (
     <RectButton {...rest}>
@@ -24,14 +25,17 @@ export function Category({ title, icon: Icon, checked = false, ...rest }: Catego
         style={styles.container}
         colors={[secondary50, secondary70]}
       >
-        <View style={[styles.content, { opacity: checked ? 1 : 0.4 }]}>
-          <View style={checked ? styles.checked : styles.check} />
+        <LinearGradient
+          style={[styles.content, { opacity: checked ? 1 : 0.5 }]}
+          colors={[checked ? secondary85 : secondary50, secondary40]}
+        >
+          {hasCheckbox && <View style={checked ? styles.checked : styles.check} />}
           <Icon width={48} height={48} />
 
           <Text style={styles.title}>
             {title}
           </Text>
-        </View>
+        </LinearGradient>
       </LinearGradient>
     </RectButton>
   );
